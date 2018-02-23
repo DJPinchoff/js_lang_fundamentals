f2793685cf7629dbe51d981683f744a1893f8795
Dave Pinchoff <dpinchoff@gmail.com>
2018-02-23 18:06:09 +0200

    Starting 01_Med

---
+++ 01_med/01_logical_operation.js
@@ -0,0 +1,14 @@
+/*
+What will each line of the following code return? Don't run the code until after you have tried to answer.
+*/
+
+console.log((false && undefined));
+console.log((false || undefined));
+console.log(((false && undefined) || (false || undefined)));
+console.log(((false || undefined) || (false && undefined)));
+console.log(((false && undefined) && (false || undefined)));
+console.log(((false || undefined) && (false && undefined)));
+console.log(('a' || (false && undefined) || ''));
+console.log(((false && undefined) || 'a' || ''));
+console.log(('a' && (false || undefined) && ''));
+console.log(((false || undefined) && 'a' && ''));
